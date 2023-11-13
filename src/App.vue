b71b80d1c721cb42cbcfe7681cf3184da7afeb45
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-11-13 22:01:07 +0300

    lessont 3_5

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,22 @@
 <template>
   <div id="app">
     <h1>{{ msg }}</h1>
+    <app-car></app-car>
   </div>
 </template>
 
 <script>
+import Car from './Car.vue'
+
+
 export default {
   data () {
     return {
       msg: 'Welcome to Your Vue.js App'
     }
+  },
+  components: {
+    appCar: Car,
   }
 }
 </script>

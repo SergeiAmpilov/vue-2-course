82816ca9587fe28032a0a9b7b898018ec5490e45
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-11-18 22:35:50 +0300

    lesson 5_3

--- src/Container.vue
+++ src/Container.vue
@@ -1,14 +1,22 @@
 <template>
   <div>
-    <h1 v-colored>Container template</h1>
+    <h1 v-if="visible" v-colored>{{ title }}</h1>
     <slot name="title"></slot>
     <hr>
     <slot name="text"></slot>
+    <button @click="visible = !visible">Hide me</button>
+    <button @click="title = 'total new title'">Change title</button>
   </div>
 </template>
 
 <script>
 export default {
+  data: function() {
+    return {
+      visible: true,
+      title: 'My first title'
+    }
+  }
 
 }
 </script>
--- src/color.js
+++ src/color.js
@@ -1,5 +1,21 @@
 export default {
   bind: function(el, bindings, vnode) {
+    console.log('bind');
     el.style.color = 'purple';
+  },
+  inserted: function(el, bindings, vnode) {
+    console.log('inserted');
+  },
+  update: function(el, bindings, vnode, oldVnode) {
+    console.log('update');
+
+  },
+  componentUpdated: function(el, bindings, vnode, oldVnode) {
+    console.log('componentUpdated');
+
+  },
+  unbind: function() {
+    console.log('unbind');
+
   }
 }

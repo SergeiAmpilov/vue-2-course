0b3ac371c1e9174639dfe276d9af81196a426309
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-11-19 18:10:22 +0300

    lesson 5_7

--- src/Container.vue
+++ src/Container.vue
@@ -2,6 +2,7 @@
   <div>
     <h1 v-colored:color.font="'green'">{{ title }}</h1>
     <p v-colored:background="'yellow'">Lorem ipsum dolor sit.</p>
+    <p v-font>Lorem ipsum dolor sit amet consectetur adipisicing elit. Eius eum, sint inventore a quia accusantium, facere iure ea cupiditate fugit recusandae reprehenderit vel qui obcaecati, dicta blanditiis provident odit asperiores amet culpa aut veritatis? Odit dignissimos eveniet natus nihil rem?</p>
     <slot name="title"></slot>
     <hr>
     <slot name="text"></slot>
@@ -14,7 +15,14 @@ export default {
     return {
       title: 'My first title'
     }
-  }
+  },
+  directives: {
+    font: {
+      bind(el, bindings, vnode) {
+        el.style.fontSize= '49px';
+      }
+    }
+  },
 
 }
 </script>

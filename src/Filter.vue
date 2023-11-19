a5ceb9a72db49b4f9fe178bb6fec5ecb3b91bc14
Sergei Ampilov <sergei.ampilov@gmail.com>
2023-11-19 19:39:37 +0300

    lesson 6_2

--- src/Filter.vue
+++ src/Filter.vue
@@ -3,6 +3,10 @@
     <h1>{{ title }}</h1>
     <h1>{{ title | lowercase }}</h1>
     <h1>{{ title | uppercase }}</h1>
+    <input type="text" v-model.lazy="searchname">
+    <ul>
+      <li v-for="name of filteredNames">{{ name }}</li>
+    </ul>
   </div>
 </template>
 
@@ -11,8 +15,17 @@
 export default {
   data: function() {
     return {
-      title: 'Hello Vue'
+      title: 'Hello Vue',
+      searchname: '',
+      names: ['Vika', 'Andrei', 'Popkova', 'a4']
     }
+  },
+  computed: {
+    filteredNames: function() {
+      return this.names.filter((element) => {
+        return element.toLocaleLowerCase().indexOf(this.searchname.toLowerCase()) !== -1;
+      })
+    },
   },
   filters: {
     lowercase: function(value) {
